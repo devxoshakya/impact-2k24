2c45f21c4fe5baa9c9f86ce6221723b470414da6
Dev Shakya <devxoshakya@gmail.com>
2024-10-17 00:03:07 +0530

    added images and hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,32 +1,49 @@
 "use client";
 import React from "react";
 import { Vortex } from "../ui/vortex";
+import { Button } from "@/components/ui/button";
+import { CoolMode } from "@/components/ui/cool.tsx";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export function Hero() {
   return (
-    (<div
-      className="min-w-full mx-auto rounded-md  h-screen overflow-hidden">
+    <div className="min-w-full mx-auto rounded-md  h-screen overflow-hidden">
       <Vortex
         backgroundColor="black"
         rangeY={800}
         particleCount={500}
         baseHue={120}
-        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full">
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+      >
         <h2 className="text-white text-5xl md:text-6xl font-bold text-center">
-            IMPACT '24
+          IMPACT '24
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
           This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
           burned and you&apos;ll have a scar.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button
-            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-            Order now
-          </button>
-          <button className="px-4 py-2  text-white ">Watch trailer</button>
+          <CoolMode>
+            <Button className="bg-blue-500 hover:bg-blue-700 text-white">
+              Register Now!
+            </Button>
+          </CoolMode>
+        </div>
+        <div className="flex flex-col mt-8 items-center gap-4 mt-6">
+          <h2 className="font-bold font-xs">Powered by</h2>
+          <div className="flex gap-4">
+            <Avatar className="w-16 h-16 bg-white">
+              <AvatarImage src="/images/CSI.png" alt="@csi" className="object-contain"/>
+            </Avatar>
+            <Avatar className="w-16 h-16 bg-white">
+              <AvatarImage src="/images/MIET.png" alt="@miet" className="object-contain p-2"/>
+            </Avatar>
+            <Avatar className="w-16 h-16 bg-cover bg-white">
+              <AvatarImage src="/images/ACIC.png" alt="@acic" className="object-contain"   />
+            </Avatar>
+          </div>
         </div>
       </Vortex>
-    </div>)
+    </div>
   );
 }
