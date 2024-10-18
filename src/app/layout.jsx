71eba080dfa0e0475a06bce01a55e48765bed018
Dev Shakya <devxoshakya@gmail.com>
2024-10-18 17:04:08 +0530

    --fix-deployment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black">
+      <body className="bg-black opacity-0">
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
